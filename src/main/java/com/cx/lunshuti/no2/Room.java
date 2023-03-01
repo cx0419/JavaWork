22af0d9903180c128f2e389e3f1712c8d70c17b8
cx <3207242935@qq.com>
2023-03-01 23:01:22 +0800

    commit1

--- src/main/java/com/cx/lunshuti/no2/Room.java
+++ src/main/java/com/cx/lunshuti/no2/Room.java
@@ -7,7 +7,7 @@ public class Room {
     public static void main(String[] args) {
         Thread reThread = new Thread(new ReceiveTask(8080));
         reThread.start();
-        Thread SeThread = new Thread(new SendTask(8081));
+        Thread SeThread = new Thread(new SendTask(8080));
         SeThread.start();
 
     }
